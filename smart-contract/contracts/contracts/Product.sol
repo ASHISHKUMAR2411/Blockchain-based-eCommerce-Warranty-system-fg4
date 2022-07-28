80865ee3aa50644f5c4048086265016102fd414c
Ashish Kumar <ashishkumar24112001@gmail.com>
2022-07-28 08:35:11 +0530

    Added smart contract, pinata for ipfs and the deployement of nft to alchemy.

---
+++ smart-contract/contracts/contracts/Product.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+// @custom:security-contact email@gmail.com
+contract Product is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
+    // we have to get rid of this Counter once we create our nfts decaying
+    using Counters for Counters.Counter;
+
+    // we can set the product prize by default, but we more will pass inside the safemint function
+    // uint256 public productPrize = 0.01 ether;
+    Counters.Counter private _tokenIdCounter;
+ 
+    constructor() ERC721("Product", "PDT") {}
+ 
+    // function _baseURI() internal pure override returns (string memory) {
+    //     return "https://ashish.com/";
+    // }
+ 
+    // to check the owner we have a builtin function in ERC721 which is ownerOf takes tokenId that is it will return the owner's address
+ 
+   
+    function safeMint(address to) public onlyOwner payable {
+        // currently we need to change the tokenId and set it to the timestam i.e block.timestamp + warrantyPeriod
+        // we require atleast the price of the product to be paid first before creating nft
+        // for that -
+        // require(msg.value >= productPrize, "Ether is not enough to buy")
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(to, tokenId);
+        // here we transfer the autheticity of the product
+        // before transfer we need to transfer the ether to the owner
+        // withdraw(); which is a function written
+        // transferOwnership(to);
+        // or we can also use safeTransferFrom(from, to, tokenId)
+    }
+ 
+    // The following functions are overrides required by Solidity.
+ 
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+ 
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+ 
+    // withdraw ether from selling the product to the owner account
+    function withdraw() public onlyOwner{
+        require(address(this).balance > 0);
+        payable(owner()).transfer(address(this).balance);
+    }
+ 
+    // for refund
+    // function refund(address _to, uint256 _tokenId, uint256 _price) public payable {
+    //     require(address(this).balance > _price, "not enough balance to refund");
+    //     payable(_to).transfer(_price);
+    //     // Now delete the nft from the blockchain network
+    //     _burn(_tokenId);
+    // }
+}
---
+++ smart-contract/contracts/hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-etherscan");
+
+// using alchemy to api to deploy the smart contract
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_API,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API,
+  },
+};
+// "ethereum-waffle": "^3.4.4",
+// 0x70cdE575D56Ce530dAc50434a812eeF257eEF6F9
